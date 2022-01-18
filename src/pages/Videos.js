c3d2b9099b7ea69ea377b602cddc8df87ef2b10c
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-01-17 18:38:33 -0600

    helo

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "../components/Navbar.js";
 import lilborshun from "../components/pics/sketches/lil_borshun.mp4";
-import promposal from "../components/pics/sketches/promposal.mp4";
+// import promposal from "../components/pics/sketches/promposal.mp4";
 import selfHelp from "../components/pics/short_skits/self_help.mov";
 import quarantine from "../components/pics/short_skits/quarantine.mp4";
 import interview from "../components/pics/short_skits/interview_with_diana.mp4";
@@ -15,7 +15,7 @@ function Videos(){
       </div>
       <div className = "row justify-content-center">
       <div className="skits text-center">
-          <h1 className="video-header text-left ml-5">Short Skits</h1>
+          <h1 className="video-subject-header text-left ml-5">Short Skits</h1>
           <h2 className="video-header">Love yourself (2018)</h2>
           <video src={selfHelp} className="vid mb-4" controls></video>
           <h2 className="video-header">Quarantine (2020)</h2>
@@ -24,9 +24,9 @@ function Videos(){
           <video src={interview} className="vid mb-4" controls></video>
         </div>
         <div className="stand-up text-center">
-          <h1 className="video-header text-left ml-5">Sketches</h1>
-          <h2 className="video-header">FHS Promposal (2017)</h2>
-          <video src={promposal} className="vid mb-4" controls></video>
+          <h1 className="video-subject-header text-left ml-5">Sketches</h1>
+          {/* <h2 className="video-header">FHS Promposal (2017)</h2>
+          <video src={promposal} className="vid mb-4" controls></video> */}
           <h2 className="video-header">Lil Borshun (2018)</h2>
           <video src={lilborshun} className="vid" controls></video>
         </div>
